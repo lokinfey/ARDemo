79ffd65c261e1e05488dbdebdc0c1bf144e38e79
Kinfey <lokinfey@outlook.com>
2017-07-28 22:50:48 +0800

    ARKitDemo
    This is arkit demo for xamarin

---
+++ ARDemo/ARViewController.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+using UIKit;
+using ARKit;
+using SceneKit;
+
+namespace ARDemo
+{
+    public partial class ARViewController : UIViewController
+	{
+		ARSCNView arSCNView;
+		ARSession arSession;
+		ARSessionConfiguration arSessionConfig;
+
+  //      public ARViewController() // : base("ARViewController", null)
+  //      {
+
+		//}
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+
+        }
+
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+
+			ARWorldTrackingSessionConfiguration config = new ARWorldTrackingSessionConfiguration();
+			config.PlaneDetection = ARPlaneDetection.Horizontal;
+			arSessionConfig = config;
+			arSessionConfig.LightEstimationEnabled = true;
+
+			arSession = new ARSession();
+
+			arSCNView = new ARSCNView();
+			arSCNView.Frame = View.Bounds;
+			arSCNView.Session = arSession;
+			arSCNView.AutomaticallyUpdatesLighting = true;
+			View.AddSubview(this.arSCNView);
+			arSession.Run(this.arSessionConfig);
+
+
+
+			var scene = SCNScene.FromFile("Models.scnassets/chair/chair.scn");
+
+			SCNNode node = scene.RootNode.ChildNodes[0];
+
+
+			node.Position = new SCNVector3(0, -1, -1);
+
+			arSCNView.Scene.RootNode.AddNodes(node);
+
+
+
+		}
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
+
---
+++ ARDemo/ViewController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using UIKit;
+
+using ARKit;
+using SceneKit;
+
+namespace ARDemo
+{
+    public partial class ViewController : UIViewController
+    {
+
+		ARSCNView arSCNView;
+		ARSession arSession;
+		ARSessionConfiguration arSessionConfig;
+
+
+
+
+
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+        }
+
+        partial void UIButton2935_TouchUpInside(UIButton sender)
+		{
+			ARViewController arViewCtrl = new ARViewController();
+
+			this.PresentViewController(arViewCtrl, true, null);
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
